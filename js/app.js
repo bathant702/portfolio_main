433a6c9e4e490a8e4279d8d6250e5b567959f26d
bathant702 <bathant702nv@gmail.com>
2025-03-24 14:22:17 -0700

    structuring portfolio

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Light/Dark Mode Toggle
 const themeToggle = document.getElementById("theme-toggle");
+
 themeToggle.addEventListener("click", () => {
+  console.log("Click");
   document.body.classList.toggle("light-mode");
 });
